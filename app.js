1114adcdb18d78338a664838994d3c78e9dfd720
inipunyaku <mfarhanfu@gmail.com>
2022-10-13 02:16:25 +0700

    add Multer

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- controller/alatberat.js
+++ controller/alatberat.js
@@ -1,139 +1,198 @@
 const model = require('../models/index');
 const axios = require('axios');
 const type = require('../models/type');
-const { json, response } = require('express');
+const {
+    json,
+    response
+} = require('express');
+const upload = require('../helper/uploader');
+const jm = require('jmerger')
 
-const getAllAlat =  async(req, res) => {
-try{
+const getAllAlat = async (req, res) => {
+    try {
 
-    const alat = await model.alat_berat.findAll({});
-    if (alat.length !== 0) {
-        res.json({
-        'status': 'OK',
-        'messages': '',
-        'data': alat
-        })
-    } else {
-        res.json({
-        'status': 'ERROR',
-        'messages': 'EMPTY',
-        'data': {}
-        })
+        const alat = await model.alat_berat.findAll({
+            raw: true,
+            nest: true
+        });
+        if (alat.length !== 0) {
+            res.json({
+                'status': 'OK',
+                'messages': '',
+                'data': alat
+            })
+        } else {
+            res.json({
+                'status': 'ERROR',
+                'messages': 'EMPTY',
+                'data': {}
+            })
+        }
+
+    } catch (err) {
+        res.status(500).json({
+            message: "Ada kesalahan",
+            error: err,
+        });
     }
-
-}catch(err){
-    res.status(500).json({
-        message: "Ada kesalahan",
-        error: err,
-    });
-}
 };
 
 const postAlat = async (req, res) => {
-  try {
-    const {kode_type, merk, status, harga, denda, operator, bbm, gambar, ket} = req.body;
-    
-    const alat = await model.alat_berat.create({
-        kode_type : kode_type, merk:merk, status:status, harga:harga, denda:denda, operator:operator, bbm:bbm, gambar:gambar, ket:ket
-    });
-    if(alat){
-        res.status(201).json({
-            'status': 'OK',
-            'messages': 'ALat Berat berhasil ditambah',
-            'data': alat
-        })
-    }
-  } catch (err) {
-    res.status(500).json({
-        message: "Ada kesalahan",
-        error: err,
-    });
-  }
-  };
+    try {
+        const {
+            kode_type,
+            merk,
+            status,
+            harga,
+            denda,
+            operator,
+            bbm,
+            ket
+        } = await req.body;
+        const foto_alat = await req.file;
 
-  const editAlat = async (req, res) => {
+        const alat = await model.alat_berat.create({
+            kode_type: kode_type,
+            merk: merk,
+            status: status,
+            harga: harga,
+            denda: denda,
+            operator: operator,
+            bbm: bbm,
+            gambar: foto_alat.filename,
+            ket: ket
+        });
+        filenameUrl = foto_alat.filename.replaceAll(" ", "%20")
+        const url = await req.protocol + "://" + req.get('host') + "/images/" + filenameUrl;
+        const url_json = await {
+            url: url
+        }
+        console.log(url, url_json)
+        const output = await jm(req.body, url_json);
+        console.log(output)
+        if (alat) {
+            res.status(201).json({
+                'status': 'OK',
+                'messages': 'ALat Berat berhasil ditambah',
+                'data': output
+            })
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "Ada kesalahan",
+            error: err,
+        });
+    }
+};
+
+const editAlat = async (req, res) => {
     try {
         const id = req.params.id;
-        const {kode_type, merk, status, harga, denda, operator, bbm, gambar, ket} = req.body;
+        const foto_alat = await req.file;
+        const {
+            kode_type,
+            merk,
+            status,
+            harga,
+            denda,
+            operator,
+            bbm,
+            gambar,
+            ket
+        } = req.body;
         const alat = await model.alat_berat.update({
-            kode_type : kode_type, merk:merk, status:status, harga:harga, denda:denda, operator:operator, bbm:bbm, gambar:gambar, ket:ket
-        },{ 
-        where: {
-            id: id
-        }
-      }
-      );
-      if(alat){
-          res.status(201).json({
-              'status': 'OK',
-              'messages': 'Alat Berat Behasil di update',
-              'data': alat
-          })
-      }
-    } catch (err) {
-      res.status(500).json({
-          message: "Ada kesalahan",
-          error: err,
-      });
-    }
-    };
-
-    const deleteAlat = async (req, res) => {
-        try {
-            const id = req.params.id;
-            const alat = await model.alat_berat.destroy({
+                kode_type: kode_type,
+                merk: merk,
+                status: status,
+                harga: harga,
+                denda: denda,
+                operator: operator,
+                bbm: bbm,
+                gambar: foto_alat.filename,
+                ket: ket
+            }, {
                 where: {
                     id: id
                 }
-            }
-          );
-          if(alat){
-              res.status(200).json({
-                  'status': 'OK',
-                  'messages': 'Alat Berat Behasil di update',
-                  'data': alat
-              })
-          }
-        } catch (err) {
-          res.status(500).json({
-              message: "Ada kesalahan",
-              error: err,
-          });
-        }
-        };
 
-        const getAlatById = async (req, res) => {
-            try {
-                const idparams = req.params.id;
-                const alat = await model.alat_berat.findOne({
-                    where: {
-                        id: idparams
-                    }
-                }
-              );
-              if(alat){
-                  res.status(200).json({
-                      'status': 'OK',
-                      'messages': 'Alat Berat ditemukan',
-                      'data': alat
-                  })
-              }
-              else{
-                res.status(404).json({
-                    'status': 'not found',
-                    'messages': 'Alat Berat tidak ditemukan',
-                    'data': alat
-                })
-              }
-            } catch (err) {
-              res.status(500).json({
-                  message: "Ada kesalahan",
-                  error: err,
-              });
             }
-            };
-    
-        
+
+        );
+        const url = await req.protocol + "//" + req.get('host') + "/images" + foto_alat.filename;
+        const url_json = await {
+            url: url
+        }
+        const output = await jm(alat, url_json);
+        console.log(output)
+
+        if (alat) {
+            res.status(201).json({
+                'status': 'OK',
+                'messages': 'Alat Berat Behasil di update',
+                'data': output
+            })
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "Ada kesalahan",
+            error: err,
+        });
+    }
+};
+
+const deleteAlat = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const alat = await model.alat_berat.destroy({
+            where: {
+                id: id
+            }
+        });
+        if (alat) {
+            res.status(200).json({
+                'status': 'OK',
+                'messages': 'Alat Berat Behasil di hapus',
+                'data': alat
+            })
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "Ada kesalahan",
+            error: err,
+        });
+    }
+};
+
+const getAlatById = async (req, res) => {
+    try {
+        const idparams = req.params.id;
+        const alat = await model.alat_berat.findOne({
+            where: {
+                id: idparams
+            }
+        });
+        if (alat) {
+            res.status(200).json({
+                'status': 'OK',
+                'messages': 'Alat Berat ditemukan',
+                'data': alat
+            })
+        } else {
+            res.status(404).json({
+                'status': 'not found',
+                'messages': 'Alat Berat tidak ditemukan',
+                'data': alat
+            })
+        }
+    } catch (err) {
+        res.status(500).json({
+            message: "Ada kesalahan",
+            error: err,
+        });
+    }
+};
+
+
 
 
 
@@ -143,5 +202,4 @@ module.exports = {
     editAlat,
     deleteAlat,
     getAlatById
-  };
-  
+};
