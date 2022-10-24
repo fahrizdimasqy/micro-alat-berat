ef950ce53285ddfa306e23fa0dab70da11534898
Fahriz Dimasqy <1197050037@student.uinsgd.ac.id>
2022-10-25 00:33:19 +0700

    done

--- routes/handler/users/logout.js
+++ routes/handler/users/logout.js
@@ -8,7 +8,7 @@ const api = apiAdapter(URL_SERVICE_USER);
 module.exports = async (req, res) => {
     try {
         const id = req.user.data.id
-        const user = await api.put(`/users/${id}`, req.body);
+        const user = await api.post(`/users/logout`, { user_id: id });
         return res.json(user.data);
     } catch (error) {
 
