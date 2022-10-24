92901b6bb7ca5872ed5c1664eb61dd6f74f56f36
Fahriz Dimasqy <1197050037@student.uinsgd.ac.id>
2022-10-24 21:07:30 +0700

    integrasi

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const refreshTokensRouter = require('./routes/refreshTokens');
+const verifyToken = require('./middlewares/verifyToken')
 
 const app = express();
 
@@ -22,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/refresh-tokens', refreshTokensRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
